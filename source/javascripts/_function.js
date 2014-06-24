fe4d6c94006a14adde58557a444d4a958108ed76
Nacho Decima <jidecima@gmail.com>
2014-06-24 20:17:16 -0300

    [Add option to slide pictures]

---
+++ source/javascripts/_function.js
@@ -0,0 +1,20 @@
+// $(window).load(function() {
+//   $('.flexslider').flexslider({
+//     animation: "slide",
+//     itemWidth: 210,
+//     itemMargin: 5,
+//     minItems: 2,
+//     maxItems: 4
+//   });
+// });
+
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide",
+    animationLoop: false,
+    itemWidth: 210,
+    itemMargin: 5,
+    minItems: 2,
+    maxItems: 4
+  });
+});
--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,2 +1,4 @@
+//= require "vendor/jquery-2.1.0.js"
+//= require "vendor/jquery.flexslider.js"
 
-//= require "function"
+//= require_tree .
